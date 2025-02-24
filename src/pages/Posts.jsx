1399f2b085b7207200bc0d82da5217ec180dcfe6
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-24 16:43:40 +0100

    [ADD] posts function

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,11 @@
+const Posts = () => {
+    return (
+      <div>
+        <h1>Lista dei Post</h1>
+        <p>Ecco tutti i nostri articoli.</p>
+      </div>
+    );
+  };
+  
+  export default Posts;
+  
