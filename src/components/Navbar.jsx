ce273aa782cd25a2bb1bdc4b6fb5c530c59ad8c3
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-24 16:48:19 +0100

    [ADD] Navbar test

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+
+// test
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/about" className={({ isActive }) => (isActive ? "active" : "")}>
+            Chi siamo
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/posts" className={({ isActive }) => (isActive ? "active" : "")}>
+            Post
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
