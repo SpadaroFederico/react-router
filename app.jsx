10b48a7e7105877943c196987e1eb99ba2f3ef11
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-24 17:01:21 +0100

    [ADD] routes in app.jsx

---
+++ app.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Posts from "./pages/Posts";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="posts" element={<Posts />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
