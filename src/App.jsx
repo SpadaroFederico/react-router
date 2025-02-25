9d213d3bfffb23c0c13de098b8db4c37b8c5ccee
Federico Spadaro <federicospadaro2@gmail.com>
2025-02-25 16:47:41 +0100

    [ADD] potdetails file + import + route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import PostDetail from "./pages/PostDetail";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="posts" element={<Posts />} />
+          <Route path="PostDetail" element={<PostDetail />} />
         </Route>
       </Routes>
     </Router>
